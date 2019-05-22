bd07b767fa9e802c95f67bed013b2279f323cb40
fadlulazmi <fadlul_azmi@yahoo.com>
2019-05-22 13:45:51 +0700

    all relations

--- models/game.js
+++ models/game.js
@@ -3,10 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Game = sequelize.define('Game', {
     level: DataTypes.STRING,
     time: DataTypes.INTEGER,
-    board: DataTypes.INTEGER
+    BoardId: DataTypes.INTEGER
   }, {});
   Game.associate = function(models) {
     // associations can be defined here
+    Game.belongsTo(models.Board)
+    Game.hasMany(models.Play)
   };
   return Game;
 };
