37f7160a3728caf87cf7aba5506f85d0bd080bb8
Elia <elia@Elias-MacBook-Air.local>
2019-05-22 13:29:59 +0700

    button ga bisa

--- app.js
+++ app.js
@@ -1,9 +1,70 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {User} = require('./models')
 
 app.use(express.urlencoded({extended: false}));
-app.use("/user", require('./routes/routeUser'))
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.get('/signin', (req, res) => {
+    res.render('user/signin.ejs', {
+        errSignIn: req.query.errMsg
+    })
+})
+
+app.post('/signin', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(user => {
+        if (user) {
+            if (user.password == req.body.password) {
+                res.send(user)
+            } else {
+                throw Error('Wrong password')
+            }
+        } else {
+            throw Error('Wrong username')
+        }
+    })
+    .catch(err => {
+        res.render('home.ejs', {err: err})
+    })
+})
+
+app.get('/signup', (req,res) => {
+    res.render('./user/signup.ejs', {
+        errSignUp: req.query.errMsg
+    })
+})
+
+app.post('/signup', (req, res) => {
+    console.log('test masuk')
+    if (req.body.repeatpass != req.body.password) {
+        throw Error('Repeat Password and Password not match')
+    } else {
+        let temp = {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        }
+        console.log(temp)
+        User.create(temp)
+        .then(success => {
+            res.send(success)
+        })
+        .catch(err => {
+            res.render('home.ejs', {err: err})
+        })
+    }
+})
+
+// app.use("/user", require('./routes/routeUser'))
 
 app.listen(port, () => 
 console.log(`Reaction Game listening on port ${port}!`))
--- routes/routeUser.js
+++ routes/routeUser.js
@@ -1,29 +1,41 @@
 const Route = require('express').Router()
 const User = require('../models').User
 
-Route.get('/login', (req, res) => {
-    User.findAll()
-    .then(allUser => {        
-        res.render('../views/user.ejs', { dataUser: allUser })
-    })
-    .catch(err => {
-        res.send(err)
-    })
-})
+// Route.get('/login', (req, res) => {
+//     User.findOne({
+//         where: {
+//             username: 'eliavictor'
+//         }
+//     })
+//     .then(user => {
+//         if (user) {
+//             res.redirect('../views/home.ejs')
+//         }
+//     })
+//     .catch(err => {
+//         res.send(err)
+//     })
+// })
 
-Route.post('/register', (req, res) => {
-    let temp = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }
-    User.create(temp)
-    .then(success => {
-        res.redirect('/user')
-    })
-    .catch(err => {
-        res.send(err)
-    })
-})
+// Route.post('/signin', (req, res) => {
+//     User.findOne({
+//         where: {
+//             username: req.body.username,
+//             password: req.body.password
+//         }
+//     })
+//     .then(user => {
+//         if (user) {
+//             res.send(user)
+//             // console.log(user.dataValues)
+//         } else {
+//             throw new Error('Username or password wrong')
+//         }
+//     })
+//     .catch(err => {
+//         res.redirect('/user/signin?errMsg=' + err.msg)
+//     })
+// })
+
 
 module.exports = Route
