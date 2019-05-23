68edfa9d52fbfa27f40c2832f45581115907d0ae
Elia <elia@Elias-MacBook-Air.local>
2019-05-23 15:53:34 +0700

    update

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       validate: {
         len: {
-          args: [8, 30],
+          args: [8, 64],
           msg: "Password minimal 8 character"
         }
       }
